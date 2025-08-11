d496e782a4fcff2e63e7991a7d32dbf9f59b63e4
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-10 21:13:31 -0400

    added method to get sample image

---
+++ load_sattelite_images.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+def download_sample():
+    # This is a small Sentinel-2 sample file
+    url = "https://github.com/sentinel-hub/eo-learn/raw/master/example_data/TestEOPatch/data_timeless/DEM/DEM.tif"
+    filename = "sample_satellite.tif"
+    
+    if not os.path.exists(filename):
+        print("Downloading sample file...")
+
+        # Step 1: Make a request to get the file
+        response = requests.get(url)
+        
+        # Step 2: Save the content to a file
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        
+        print(f"Downloaded {filename}")
+    else:
+        print("Sample file already exists!")
+    return filename
