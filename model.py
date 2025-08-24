6e93a31416385e363517943b0cdc799091498b42
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-08-23 21:31:47 -0400

    defined in and out model struct

--- model.py
+++ model.py
@@ -12,8 +12,24 @@ print(f"  Type: {type(model.conv1)}")
 print(f"  Input channels: {model.conv1.in_channels}")
 print(f"  Output channels: {model.conv1.out_channels}")
 
+# Input model
+model.conv1 = nn.Conv2d(
+    in_channels=3,          # this is needed to match the number of input channels of the model
+    out_channels=64, 
+    kernel_size=7, 
+    stride=2, 
+    padding=3, 
+    bias=False
+    )
+
 # look at the output layer  
 print("\nOriginal OUTPUT layer:")
 print(f"  Type: {type(model.fc)}")
 print(f"  Input features: {model.fc.in_features}")
 print(f"  Output classes: {model.fc.out_features}")
+
+# Output model
+model.fc = nn.Linear(
+    in_features=model.fc.in_features,
+    out_features=8         # this is needed to match the number of output classes
+)
